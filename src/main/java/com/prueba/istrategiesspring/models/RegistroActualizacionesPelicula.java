90112f77415c1ce11c26c3e345eaba956da2cf4a
Raúl Granados <raul.granados12@gmail.com>
2021-04-28 17:04:20 -0600

    Agregando clase de registro

---
+++ src/main/java/com/prueba/istrategiesspring/models/RegistroActualizacionesPelicula.java
@@ -0,0 +1,15 @@
+package com.prueba.istrategiesspring.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+//@Entity
+public class RegistroActualizacionesPelicula {
+
+    /*@Id
+    @GeneratedValue
+    private Long id;*/
+
+
+}
